161f4aeacae58bb83776ecdca46f30de5cedd43b
Nishant Dobariya <nishantdobariya@outlook.com>
2025-01-15 18:59:06 +0530

    env

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,10 @@
 export const env = {
   PORT: Number(process.env.REACT_APP_PORT) || 5000,
-  USER_SERVICE_URL: process.env.REACT_APP_USER_SERVICE_URL || "",
-  POST_SERVICE_URL: process.env.REACT_APP_POST_SERVICE_URL || "",
-  GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID || "",
+  USER_SERVICE_URL: "https://api.user.activatree.com" || "",
+  POST_SERVICE_URL: "https://api.post.activatree.com" || "",
+  GOOGLE_CLIENT_ID:
+    "274136206982-naj76ba4l49nqieh60ce0o4lkep704n3.apps.googleusercontent.com" ||
+    "",
   GOOGLE_CLIENT_SECRETE: process.env.REACT_APP_GOOGLE_CLIENT_SECRETE || "",
-  GOOGLE_REDIRECT_URL: process.env.REACT_APP_GOOGLE_REDIRECT_URL || "",
+  GOOGLE_REDIRECT_URL: "https://activatree.com/auth/callback" || "",
 };
